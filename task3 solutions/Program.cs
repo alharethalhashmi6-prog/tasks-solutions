e37bfca4b917954972a07737e7c99b02d547d75a
Alharith <alharethalhashmi6@gmail.com>
2026-07-06 15:15:17 +0400

    add new file

---
+++ task3 solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task3_solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
