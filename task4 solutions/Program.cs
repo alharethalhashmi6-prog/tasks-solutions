081c7fc2aaa9fde69032b96faa4d574f34964a87
Alharith <alharethalhashmi6@gmail.com>
2026-07-08 11:14:28 +0400

    Q2

--- task4 solutions/Program.cs
+++ task4 solutions/Program.cs
@@ -17,5 +17,29 @@
             PrintWelcome(userName);
         }
         */
+
+
+        //----------------------------------------------
+
+        /*
+        // task 2
+
+         static int Square(int number)
+         {
+             return number * number;
+         }
+         static void Main(string[] args)
+         {
+             Console.Write("Enter a number: ");
+             int num = int.Parse(Console.ReadLine());
+
+             int result = Square(num);
+             Console.WriteLine("The square is: " + result);
+         }
+        */
+
+        //----------------------------------------------
+        
+
     }
 }
