2943fe519c53a462b6a351968d364a7d2d223b30
Alharith <alharethalhashmi6@gmail.com>
2026-07-12 23:17:51 +0400

    Q1

--- task5 solutions/Program.cs
+++ task5 solutions/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            /*
+            // task 1
+
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nAll grades entered:");
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            */
+
+            //-------------------------------------------------------
+
+
+
         }
+
     }
 }
