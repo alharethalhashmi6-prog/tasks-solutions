d9dea3f1d9f0f64a134faaf3fe43b5258a369f68
Alharith <alharethalhashmi6@gmail.com>
2026-07-12 23:19:36 +0400

    Q2

--- task5 solutions/Program.cs
+++ task5 solutions/Program.cs
@@ -25,6 +25,23 @@
 
             //-------------------------------------------------------
 
+            /*
+            // task 2
+
+                Stack<string> history = new Stack<string>();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write("Enter website URL " + (i + 1) + ": ");
+                    string url = Console.ReadLine();
+                    history.Push(url);
+                }
+
+                string previousPage = history.Pop();
+                Console.WriteLine("\nYou pressed 'Back'. You landed on: " + previousPage);
+            */
+
+            //-------------------------------------------------------
 
 
         }
